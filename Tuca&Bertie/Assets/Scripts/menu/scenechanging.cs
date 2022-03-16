ecbb9e96dff57195bc8a90f395d8cb79d7f64848
carambola64 <nguyenbrian68@gmail.com>
2022-03-16 16:12:28 -0400

    UI has a button to switch scenes.

---
+++ Tuca&Bertie/Assets/Scripts/menu/scenechanging.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechanging : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onVN()
+    {
+        SceneManager.LoadScene("dialogueAnimation");
+    }
+
+    public void onBuild()
+    {
+        SceneManager.LoadScene("testCreation");
+    }
+}
