76b814cbaf09ec5db2e672f176db0138a23c374c
Liam Leverock <leverockliam@gmail.com>
2022-04-16 18:43:16 -0400

    Update
    - Fixes to Room Selector
    - Started Buying Rooms

---
+++ Tuca&Bertie/Assets/Scripts/Player/RoomLocker.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLocker : MonoBehaviour
+{
+
+    public bool isRoomUnlocked = false;
+
+    public GameObject unlockRoomPrefab;
+
+    public Sprite lockedSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRoomUnlocked)
+        {
+            spriteRenderer.sprite = null;
+        } else
+        {
+            spriteRenderer.sprite = lockedSprite;
+        }
+    }
+
+    public void DisplayBuyMenu()
+    {
+        //TODO Instantiate Menu Object to Buy Room
+    }
+}
